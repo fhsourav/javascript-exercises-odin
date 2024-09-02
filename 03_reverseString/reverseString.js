05429749dfef1bd325946ee0672368205edaf8c8
Fahim <69208613+fhsourav@users.noreply.github.com>
2024-09-02 22:28:45 +0600

    Solved Exercise 03 - Reverse a String

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,9 @@
-const reverseString = function() {
-
+const reverseString = function(text) {
+	let newText = '';
+	for (let i = text.length - 1; i >= 0; i--) {
+		newText = newText.concat(text.charAt(i));
+	}
+	return newText;
 };
 
 // Do not edit below this line
