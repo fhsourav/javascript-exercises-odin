b5192281544881973024f009e71e490abb6c4d17
Fahim <69208613+fhsourav@users.noreply.github.com>
2024-09-02 22:18:39 +0600

    Solved Exercise 02 - repeatString

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
-
+const repeatString = function(givenString, repeat) {
+	if (repeat < 0) {
+		return 'ERROR';
+	}
+	let finalString = '';
+	for (let i = 0; i < repeat; i++) {
+		finalString += givenString;
+	}
+	return finalString;
 };
 
 // Do not edit below this line
