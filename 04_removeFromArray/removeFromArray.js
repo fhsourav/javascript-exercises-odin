f052d237ffa812cbd6dcb06db4985e428760818d
Fahim <69208613+fhsourav@users.noreply.github.com>
2024-09-03 14:32:43 +0600

    Solved Exercise 04 - removeFromArray

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,17 @@
-const removeFromArray = function() {
+const removeFromArray = function(givenarray, ...args) {
+	let newArray = []
+	for (let index = 0; index < givenarray.length; index++) {
+		let match = false;
+		for (let arg of args) {
+			if (typeof(givenarray[index]) == typeof(arg) && givenarray[index] == arg) {
+				match = true;
+			}
+		}
+		if (!match) {
+			newArray.push(givenarray[index]);
+		}
+	}
+	return newArray;
 };
 
 // Do not edit below this line
