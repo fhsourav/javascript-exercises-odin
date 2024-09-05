dc471456af83fcd26a26c4a40b3693a9fa041f1e
Fahim <69208613+fhsourav@users.noreply.github.com>
2024-09-06 05:39:48 +0600

    Solved Exercise 09 - Palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,26 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+	const alphabet = "abcdefghijklmnopqrstuvwxyz0123456789"
+	str = str.toLowerCase();
+	let forwardCount = 0;
+	let backwardCount = str.length - 1;
+	while (forwardCount < backwardCount) {
+		let chForward = str.charAt(forwardCount);
+		let chBackward = str.charAt(backwardCount);
+		if (!alphabet.includes(chForward)) {
+			forwardCount++;
+			continue;
+		}
+		if (!alphabet.includes(chBackward)) {
+			backwardCount--;
+			continue;
+		}
+		if(chForward != chBackward) {
+			return false;
+		}
+		forwardCount++;
+		backwardCount--;
+	}
+	return true;
 };
 
 // Do not edit below this line
